4cf94ff276ffdaa9c53d35d4a29cd6ff709b6e9a
SN1004 <jatinneekhra4@gmail.com>
2022-06-27 21:48:03 +0530

    Created lost(item) model

--- Lost_and_Found_App/LandF/models.py
+++ Lost_and_Found_App/LandF/models.py
@@ -1,12 +1,29 @@
+from pickle import TRUE
+from typing_extensions import Required
 from django.db import models
+from django.forms import ModelForm
 from django.utils import timezone
 import datetime
 
 # Create your models here.
 
 class lost(models.Model):
-    # picture
-    # description
-    # contact
-    def __str__(self) -> str:
-        return super().__str__()
+    picture = models.ImageField(upload_to = 'media')
+    
+    # TYPE_OF_LOST={
+    #     ('ID','ID'),
+    #     ('EL','Electronics'),
+    #     ('OT','Other')
+    # }
+    # Type = models.CharField(
+    #     max_length= 2,
+    #     choices= TYPE_OF_LOST,
+    #     default= 'OT',
+    # )
+
+    name = models.CharField(max_length= 15, required= True)
+    description = models.CharField(max_length= 200)
+    contact = models.CharField(max_length= 100, required = True)
+    def __str__(self):
+        return self.name
+
