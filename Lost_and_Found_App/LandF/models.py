ecc59d7c2b639aa6042ba7ce92b3ecd08eed6eba
SN1004 <jatinneekhra4@gmail.com>
2022-06-27 23:04:08 +0530

    update

--- Lost_and_Found_App/LandF/models.py
+++ Lost_and_Found_App/LandF/models.py
@@ -1,14 +1,14 @@
-from pickle import TRUE
-from typing_extensions import Required
+from django import forms
 from django.db import models
-from django.forms import ModelForm
 from django.utils import timezone
 import datetime
 
+from pkg_resources import require
+
 # Create your models here.
 
-class lost(models.Model):
-    picture = models.ImageField(upload_to = 'media')
+class Lost(models.Model):
+    picture = models.ImageField(upload_to = 'media', default='None')
     
     # TYPE_OF_LOST={
     #     ('ID','ID'),
@@ -21,9 +21,14 @@ class lost(models.Model):
     #     default= 'OT',
     # )
 
-    name = models.CharField(max_length= 15, required= True)
-    description = models.CharField(max_length= 200)
-    contact = models.CharField(max_length= 100, required = True)
+    name = models.CharField(max_length= 15, default='None')
+    description = models.CharField(max_length= 200, default= 'None')
+    contact = models.CharField(max_length= 100, default='None')
     def __str__(self):
         return self.name
+
+class LostForm(forms.ModelForm):
+    class Meta:
+        model = Lost
+        fields = '__all__'
 
--- Lost_and_Found_App/LandF/views.py
+++ Lost_and_Found_App/LandF/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
 
+from .models import Lost
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
+class IndexView(generic.ListView):
+    template_name = 'index.html'
+    context_object_name = 'latest_lost_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Lost.objects.order_by('-pub_date')[:5]
+
+def lostpost(request):
+    if request == 'POST':
+        return ()
