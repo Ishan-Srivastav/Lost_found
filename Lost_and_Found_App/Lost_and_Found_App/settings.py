f034be46bf587ba03a50d78cf3232bde695743a2
Mr.Vegeta <omprakash.prayas56jee19@gmail.com>
2022-06-29 14:03:49 +0530

    updated pip and installed modules
    pillow module installed

--- Lost_and_Found_App/Lost_and_Found_App/settings.py
+++ Lost_and_Found_App/Lost_and_Found_App/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'LostAndFound',
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': 'adminhimalik',
         'HOST': 'localhost',
     }
 }
